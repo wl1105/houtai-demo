5ed6004e04d5754531a24f439d6a79f4b4fdbeaf
wl1105 <1031270528@qq.com>
2019-05-05 20:51:24 +0800

    更新Redux DevTools扩展代码

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
-import { createStore,applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import reducer from './reducers/index.js';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk'
 
-const store = createStore(
-  reducer,
-  applyMiddleware(thunk,logger)
-  );   //仓库的实例对象
 
-export default store;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+export default createStore(
+  reducer,
+  composeEnhancers(
+    applyMiddleware(thunk, logger)
+  )
+);   //仓库的实例对象
